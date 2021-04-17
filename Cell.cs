75bd0bdcb94b9e2063dccf1a6f4a77fe67bedeae
Zachary Miller <70243304+SovietDoge47@users.noreply.github.com>
2021-04-17 13:09:26 -0500

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Cell
+{
+	public int RowNumber { get; set; }
+    public int ColumnNumber { get; set; }
+    public bool OccupiedByWhite { get; set; }
+    public bool LegalNextMove { get; set; }
+    public bool notOccupied { get; set; }
+    public Cell forwardLeft { get; set; }
+    public Cell forwardRight { get; set; }
+    public Cell backLeft { get; set; }
+    public Cell backRight { get; set; }
+
+    public Cell(int column, int row)
+    {
+        RowNumber = row;
+        ColumnNumber = column;
+        OccupiedByWhite = false;
+        LegalNextMove = false;
+        notOccupied = true;
+    }
+
+}
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //static int theBoardSize = 8;
+        //static Board theBoard = new Board(theBoardSize);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
